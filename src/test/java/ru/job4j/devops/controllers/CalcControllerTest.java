63428380d1dc21538801fa881810302273d9c9da
XLoganxmenX <loganxmen97@gmail.com>
2025-02-11 23:10:54 +0900

    add new test and gradle buildCache

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -28,10 +28,19 @@ class CalcControllerTest {
     }
 
     @Test
-    public void whenZeroPlusZero() {
+    public void whenZeroPlusNumber() {
         var input = new TwoArgs(0, 3);
         var expected = new Result(3);
         var output = new CalcController().summarise(input);
+        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(output.getBody()).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenZeroPlusZero() {
+        var input = new TwoArgs(0, 0);
+        var expected = new Result(0);
+        var output = new CalcController().times(input);
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
