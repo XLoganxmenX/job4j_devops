6eeb3f111d1b5fbe9e06158c70497c0fc9d60265
XLoganxmenX <loganxmen97@gmail.com>
2025-07-12 13:35:10 +0900

    fix (add CalcEvent repository, service and integration test) - runs only integration test

--- src/test/java/ru/job4j/devops/CalcApplicationTest.java
+++ src/test/java/ru/job4j/devops/CalcApplicationTest.java
@@ -1,9 +1,11 @@
 package ru.job4j.devops;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class CalcApplicationTests {
 
     @Test
